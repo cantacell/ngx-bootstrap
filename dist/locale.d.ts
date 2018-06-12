369801d8a0825cea83986396ecf27a2a3dcd348e
Francesco Cantarella <fcantarella@trovimap.com>
2018-06-12 11:29:35 +0200

    commit dist

---
+++ dist/locale.d.ts
@@ -0,0 +1,5 @@
+export { deLocale } from './chronos/i18n/de';
+export { enGbLocale } from './chronos/i18n/en-gb';
+export { esLocale } from './chronos/i18n/es';
+export { frLocale } from './chronos/i18n/fr';
+export { itLocale } from './chronos/i18n/it';
